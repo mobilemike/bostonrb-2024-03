d90c968184b0d1a15f122a0edd7c252a1b15db62
Mike Karolow <mobilemike@gmail.com>
2024-03-08 16:43:38 +0000

    Draft intro to ruby file

---
+++ intro_to_ruby.rb
@@ -0,0 +1,29 @@
+## Data Types
+# String
+
+
+# Integer
+
+
+# Boolean
+
+
+### Data Structures
+# Array
+
+
+# Hash
+
+
+## Using Data
+# Variables
+
+
+# Conditionals
+
+
+# Looping
+
+
+# Methods
+
